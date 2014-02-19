b0e631eaf7c4d93fae8afed931d419a908d557a0
Ben Jarlett <emailme@benjarlett.co.uk>
2014-02-19 23:44:30 +0000

    Front page theming changes

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -65,7 +65,14 @@
 <?php print $polyfills; ?>
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=1444535002449540";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
   <div id="main" class="container">
 	<div id="skip-link">
 		<ul>
